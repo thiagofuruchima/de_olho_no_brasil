ae2650fb3572f57c513cafb51dabb3ce4c4164bb
Thiago Furuchima <thiago.furuchima@gmail.com>
2023-02-22 21:06:28 -0300

    Unecessary info removed

--- db_test.py
+++ db_test.py
@@ -1,13 +1,12 @@
-import psycopg2
 from sqlalchemy import create_engine, MetaData
 import os
 
-
 if __name__ == '__main__':
-    # print(os.environ.get('TWITTER_ANALYTICS_DB_URL'))
-    engine = create_engine('postgresql+psycopg2://thiagomf:bq9E5ExCwj7Xb@postgres-minha-nuvem-db.minhanuvem.org:8081/tweet_analytics')
+    conn = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
-    metadata_obj = MetaData()
-    metadata_obj.reflect(bind=engine)
-    for table in metadata_obj.sorted_tables:
-         print(table)
+    if conn is not None and conn != '':
+        engine = create_engine(conn)
+        metadata_obj = MetaData()
+        metadata_obj.reflect(bind=engine)
+        for table in metadata_obj.sorted_tables:
+             print(table)
