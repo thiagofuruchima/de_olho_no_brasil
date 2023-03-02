0f42676aa4418e6e0c6a8d4f2923fdc700bc23c0
Thiago Furuchima <thiago.furuchima@gmail.com>
2023-03-02 18:44:03 -0300

    Layout adjustments

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ import os
 
 
 def create_app(mode='dev'):
-    # create the extension
-    db = SQLAlchemy()
-
     # create the app
     app = Flask(__name__, instance_relative_config=True)
 
@@ -16,26 +13,22 @@ def create_app(mode='dev'):
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('SQLALCHEMY_DATABASE_URI')
     app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {"pool_pre_ping": True}
 
-    if (app.config["SQLALCHEMY_DATABASE_URI"] is None) or (app.config["SQLALCHEMY_DATABASE_URI"] == ''):
-        print("URL do Banco de Dados não encontrada.")
-        exit()
-
-    # initialize the app with the extension
-    db.init_app(app)
-
     # Set the secret key to some random bytes. Keep this really secret!
     app.secret_key = os.environ.get('TWITTER_ANALYTICS_SECRET_KEY')
+
+    if (app.config["SQLALCHEMY_DATABASE_URI"] is None) or (app.config["SQLALCHEMY_DATABASE_URI"] == ''):
+        print("URL do Banco de Dados não encontrada.")
+        exit(-1)
 
     if (app.secret_key is None) or (app.secret_key == ''):
         print("Chave Secreta não encontrada.")
         exit(-1)
 
-    # override default config values from config.py file located in "instance" folder
-    is_file_configured = app.config.from_pyfile('config.py', silent=True)
+    # create the Database extension
+    db = SQLAlchemy()
 
-    if not is_file_configured:
-        # set default config vaules
-        app.config.from_mapping()
+    # initialize the app with the extension
+    db.init_app(app)
 
     @app.route('/', methods=['POST', 'GET'])
     def home():
@@ -45,51 +38,75 @@ def create_app(mode='dev'):
         elif request.method == 'POST':
             return update()
         else:
-            raise Exception("É necessário selecionar um item da lista!")
-            return render_template('home.html')
+            raise Exception("Erro ao processar a solicitação.")
 
     @app.route('/about')
     def about():
         return render_template('about.html')
 
-    def update():
-        if request.method == 'POST':
-            sql = '''SELECT tweet_id, sentiment_label, tweet_created_at, matching_rules_tag FROM tweet_analytics where matching_rules_tag=\'{}\''''
-
-            tema = request.form['tema']
-
-            print("tema: {}".format(tema))
-
-            if (tema is None) or (tema == ''):
-                raise Exception("É necessário selecionar um item da lista!")
-
-            results = db.session.execute(text(sql.format(tema)))
-
-            df = pd.DataFrame(results.mappings().all())
-
-            df_total_quantities = pd.DataFrame(df['sentiment_label'].value_counts()).sort_index(
-                ascending=False).reset_index().rename({'index': 'sentiment', 'sentiment_label': 'value'}, axis=1)
-
-            df_total_percentages = pd.DataFrame(df['sentiment_label'].value_counts(normalize=True)).sort_index(
-                ascending=False).reset_index().rename({'index': 'sentiment', 'sentiment_label': 'value'}, axis=1)
-
-            df.set_index('tweet_created_at', inplace=True)
-            df_time_series = \
-                df.resample('d')['sentiment_label'].value_counts(normalize=True).unstack('sentiment_label')[
-                    ['positive', 'neutral', 'negative']]
-
-            df_sentiment_quantities = df.resample('d')['sentiment_label'].value_counts().unstack('sentiment_label')[
-                ['positive', 'neutral', 'negative']]
-
-        return render_template('result.html', tema=tema,
-                               df_time_series=df_time_series,
-                               df_sentiment_quantities=df_sentiment_quantities,
-                               df_total_quantities=df_total_quantities,
-                               df_total_percentages=df_total_percentages)
-
     @app.errorhandler(Exception)
     def handle_exception(e):
         flash(str(e))
         return render_template('home.html')
+
+    def update():
+        if request.method == 'GET':
+            raise Exception("Erro ao processar a consulta. Informações obrigatórias não encontradas.")
+
+        tema = request.form['tema']
+
+        # Log the request type
+        app.logger.info("Consulta para o tema: {}".format(tema))
+
+        if (tema is None) or (tema == ''):
+            raise Exception("É necessário selecionar um item da lista!")
+
+        sql = '''SELECT 
+                 sum( (sentiment_label = 'positive')::int ) as qtd_positive ,
+                 sum( (sentiment_label = 'negative')::int ) as qtd_negative,
+                 sum( (sentiment_label = 'neutral')::int )  as qtd_neutral,
+                 avg( (sentiment_label = 'positive')::int ) * 100 as percent_positive ,
+                 avg( (sentiment_label = 'negative')::int ) * 100 as percent_negative,
+                 avg( (sentiment_label = 'neutral')::int )  * 100 as percent_neutral,
+                 date(tweet_created_at),
+                 matching_rules_tag
+                 FROM tweet_analytics 
+                 WHERE matching_rules_tag = \'{}\'
+                 GROUP by date(tweet_created_at), matching_rules_tag
+                 ORDER by date
+                '''
+
+        df = pd.DataFrame(db.session.execute(text(sql.format(tema))).mappings().all())
+        df.set_index('date', inplace=True)
+
+        total = df[['qtd_positive', 'qtd_negative', 'qtd_neutral']].sum().sum()
+
+        return render_template('result.html', result={'tema': tema,
+                                                      'total': total,
+                                                      'df': df})
+
+    @app.route('/compare_all')
+    def compare_all():
+
+        sql = '''SELECT 
+                 sum( (sentiment_label = 'positive')::int ) as qtd_positive ,
+                 sum( (sentiment_label = 'negative')::int ) as qtd_negative,
+                 sum( (sentiment_label = 'neutral')::int )  as qtd_neutral,
+                 avg( (sentiment_label = 'positive')::int ) * 100 as percent_positive ,
+                 avg( (sentiment_label = 'negative')::int ) * 100 as percent_negative,
+                 avg( (sentiment_label = 'neutral')::int )  * 100 as percent_neutral,
+                 date(tweet_created_at),
+                 matching_rules_tag
+                 FROM tweet_analytics 
+                 WHERE matching_rules_tag in ('Economia', 'Educação', 'Imposto de Renda', 'Saúde', 'Segurança', 'Sistema Único de Saúde')
+                 GROUP by date(tweet_created_at), matching_rules_tag
+                 ORDER by date
+                '''
+
+        result = pd.DataFrame(db.session.execute(text(sql)).mappings().all())
+        result.set_index('date', inplace=True)
+
+
+        return render_template('compare_all.html', result=result)
 
     return app
